970c3ef02a3f2f6f7c35de9e3a2cdb382569a724
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-03-11 22:17:23 +0200

    toggle (second parametr)

--- js/task-06.js
+++ js/task-06.js
@@ -10,4 +10,14 @@ const onInput = ({ currentTarget: curTarg }) => {
 
 }
 
-inputRef.addEventListener('blur', onInput);
+// inputRef.addEventListener('blur', onInput);
+
+// const inputEl = document.querySelector("#validation-input");
+// inputEl.addEventListener("blur", (event) => {
+//   const input = event.target.value.length;
+//   const validation = Number(event.target.dataset.length);
+//   event.target.classList.toggle("valid", input === validation);
+//   event.target.classList.toggle("invalid", input !== validation);
+// });
+
+// If included, turns the toggle into a one way-only operation. If set to false, then token will only be removed, but not added. If set to true, then token will only be added, but not removed.
